3190fbbdb261c25a13d303a33ea7122f9058882b
khushiiakter <rkjia1311@gmail.com>
2024-12-05 23:10:02 +0600

    get data form client site

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    
+
+    app.post('/movie', async (req, res) => {
+      const newMovie = req.body;
+      console.log('Adding new movie', newMovie);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
